5158a8576ffc47743f1af6ed1c69826ce3fbb8a3
xcepotx <alex.solachuddin@gmail.com>
2026-05-25 12:36:45 +0000

    refactor: extract response plan view component

---
+++ apps/web/src/views/ResponsePlanView.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function ResponsePlanView({
+  responsePlan,
+  selectedResponseItem,
+  setSelectedResponseItemId,
+  busy,
+  updateResponseItem,
+  generateResponsePlan,
+  requirements,
+}
+
+export default ResponsePlanView;
